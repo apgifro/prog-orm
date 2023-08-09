6169a849335c67e8e3b4b51020435569b621154d
alexgirardello <alexandre.girardello@estudante.ifro.edu.br>
2023-08-09 15:22:01 -0400

    (views) Aluno & Turma

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 
-from .views import TurmaReadView
+import views
 
 urlpatterns = [
-    path("turma/", TurmaReadView.as_view(), name="turma_read"),
-    path("turma/criar/", TurmaCreateView.as_view(), name="turma_create"),
-    # path("turma/atualizar/", TurmaUpdateView.as_view(), name="turma_update"),
-    # path("turma/excluir/", TurmaDeleteView.as_view(), name="turma_delete"),
+    path('', views.FaculdadeIndexView.as_view(), name='index'),
+
+    path("turma/", views.TurmaReadView.as_view(), name="turma_read"),
+    path("aluno/", views.AlunoReadView.as_view(), name="aluno_read"),
+
+    path("turma/criar/", views.TurmaCreateView.as_view(), name="turma_create"),
+    path("aluno/criar/", views.AlunoCreateView.as_view(), name="aluno_create"),
+
+    path("turma/atualizar/<int:pk>/", views.TurmaUpdateView.as_view(), name="turma_update"),
+    path("aluno/atualizar/<int:pk>/", views.AlunoUpdateView.as_view(), name="aluno_update"),
+
+    path("turma/excluir/<int:pk>/", views.TurmaDeleteView.as_view(), name="turma_delete"),
+    path("aluno/excluir/<int:pk>/", views.AlunoDeleteView.as_view(), name="aluno_delete"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,10 +1,71 @@
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView, DeleteView, TemplateView
 from django.views.generic.list import ListView
 
-from core.models import Turma
+from core.models import Turma, Aluno
+
+
+class FaculdadeIndexView(TemplateView):
+    template_name = 'index.html'
 
 
 class TurmaReadView(ListView):
     model = Turma
     context_object_name = 'turmas'
     template_name = 'turma/read.html'
+    ordering = ['-id']
+
+
+class TurmaCreateView(SuccessMessageMixin, CreateView):
+    model = Turma
+    fields = ["nome", "turno"]
+    template_name = 'turma/create.html'
+    success_url = reverse_lazy("turma_read")
+    success_message = "%(nome)s criado com sucesso!"
+
+
+class TurmaUpdateView(SuccessMessageMixin, UpdateView):
+    model = Turma
+    fields = ["nome", "turno"]
+    template_name = 'turma/update.html'
+    success_url = reverse_lazy("turma_read")
+    success_message = "%(nome)s atualizado com sucesso!"
+
+
+class TurmaDeleteView(SuccessMessageMixin, DeleteView):
+    model = Turma
+    template_name = 'turma/delete.html'
+    success_url = reverse_lazy("turma_read")
+    success_message = "Ecluído com sucesso!"
+
+
+class AlunoReadView(ListView):
+    model = Aluno
+    context_object_name = 'alunos'
+    template_name = 'aluno/read.html'
+    ordering = ['-id']
+
+
+class AlunoCreateView(SuccessMessageMixin, CreateView):
+    model = Aluno
+    fields = ["nome", "sobrenome", "endereco", "dtnasc", "turma"]
+    template_name = 'aluno/create.html'
+    success_url = reverse_lazy("aluno_read")
+    success_message = "%(nome)s criado com sucesso!"
+
+
+class AlunoUpdateView(SuccessMessageMixin, UpdateView):
+    model = Aluno
+    fields = ["nome", "sobrenome", "endereco", "dtnasc", "turma"]
+    template_name = 'aluno/update.html'
+    success_url = reverse_lazy("aluno_read")
+    success_message = "%(nome)s atualizado com sucesso!"
+
+
+class AlunoDeleteView(SuccessMessageMixin, DeleteView):
+    model = Aluno
+    template_name = 'aluno/delete.html'
+    success_url = reverse_lazy("aluno_read")
+    success_message = "Excluído com sucesso!"
 
